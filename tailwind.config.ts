3e5bf772cdd1fa4353ffe38d7403363494595109
thecode-master-sy <thecodemasster.com>
2024-01-27 03:44:34 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "var(--background-color)",
+        foreground: "var(--foreground-color)"
       },
+      fontSize: {
+        header: "var(--fs-hero-header)"
+      }
     },
   },
   plugins: [],
